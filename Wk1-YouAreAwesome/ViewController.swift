35ad060b2ea28f462d12c9ddec9e90bbb61d3a6e
Will Templeton <templetw@bc.edu>
2020-01-19 17:47:10 -0500

    Added Show Another Message button, image, button linked to image

--- Wk1-YouAreAwesome/ViewController.swift
+++ Wk1-YouAreAwesome/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("View Did Load!")
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showMessageButtonPressed(_ sender: UIButton) {
+        print("show Message Button Pressed")
+        messageLabel.text = "You Are Awesome!"
+        imageView.image = UIImage(named: "image0") 
+    }
+    @IBAction func showMessageButtonTwoPressed(_ sender: UIButton) {
+        print("show Message Button Two Pressed")
+        messageLabel.text = "You Are Great!"
+    }
+    
 
 }
 
